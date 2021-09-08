ce9ef009e5ac909cadbd2042cb08da57a20b03be
Nick Ball <74800360+nmb-paperspace@users.noreply.github.com>
2021-09-08 14:13:14 -0700

    Add files via upload

--- nlp_text_generation.py
+++ nlp_text_generation.py
@@ -1,5 +1,11 @@
+pip install -U transformers
+
 from transformers import pipeline, set_seed
+
 generator = pipeline('text-generation', model='gpt2')
+
 set_seed(42)
+
 generator("Hello, I'm a language model,", max_length=30, num_return_sequences=5)
+
 print('Done')
