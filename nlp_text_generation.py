09180796187c57bee27c8ec3cab89bab01fb575a
Nick Ball <74800360+nmb-paperspace@users.noreply.github.com>
2021-09-08 14:42:14 -0700

    Add files via upload

--- nlp_text_generation.py
+++ nlp_text_generation.py
@@ -1,9 +1,20 @@
+# NLP text generation
+#
+# Uses GPT-2 model from HuggingFace: https://huggingface.co/gpt2
+#
+# Last updated: Sep 08th 2021
+
+# Setup
 from transformers import pipeline, set_seed
 
+# Create text generator
 generator = pipeline('text-generation', model='gpt2')
 
+# Random seed for text generation
 set_seed(42)
 
-generator("Hello, I'm a language model,", max_length=30, num_return_sequences=5)
+# Generate text and output to file
+output = generator("Hello, I'm a language model,", max_length=30, num_return_sequences=5)
+print(output)
 
 print('Done')
